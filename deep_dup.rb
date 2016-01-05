3adc171cfff461b8f7225b26b5113853cd815bef
Kit Langton <kitlangton@gmail.com>
2016-01-05 13:29:18 -0500

    Deep dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,3 +1,12 @@
-def deep_dup
-
+class Array
+  def deep_dup
+    self.map do |element|
+      if element.is_a? Array
+        element.dup
+        element.deep_dup
+      else
+        element
+      end
+    end
+  end
 end
