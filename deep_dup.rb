d187da336ddf5fd8519970bea241861ed6521faa
Sam O'Keefe <samok13@gmail.com>
2016-01-05 09:29:57 -0800

    deep_dup, ttt_interfaces

---
+++ deep_dup.rb
@@ -0,0 +1,3 @@
+def deep_dup
+
+end
---
+++ ttt_interfaces.rb
@@ -0,0 +1,35 @@
+# class TicTacToe
+#   self.new
+#   play
+#   check_game_over
+#   check_victory
+#   check_draw
+#   switch_players
+# end
+
+# class Player
+#   self.new
+#   name
+#   piece
+#   name=
+#   piece=
+#   get_coordinates
+#   ask_for_coordinates
+#   validate_cordinates_format
+# end
+
+# class Board
+#   self.new
+#   render
+#   add_piece
+#   piece_location_valid?
+#   within_valid_coordinates?
+#   coordinates_available?
+#   winning_combination?
+#   winning_diagonal?
+#   winning_vertical?
+#   winner_horizontal?
+#   diagonals
+#   verticals
+#   horizontals
+#   full?
